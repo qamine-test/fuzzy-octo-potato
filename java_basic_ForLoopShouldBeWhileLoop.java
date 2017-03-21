bbe3d95b5b26982fa0a9d8db0f2555ab3a2c9218
Rodrigo Fernandes <rtfrodrigo@gmail.com>
2017-03-21 16:54:42 +0000

    Add rule with issues

--- java_basic_ForLoopShouldBeWhileLoop.java
+++ java_basic_ForLoopShouldBeWhileLoop.java
@@ -1,7 +1,5 @@
+//#Patterns: java_basic_ForLoopShouldBeWhileLoop
 public class Foo {
-
-    public boolean myBoolean = false;
-
     void bar() {
         //#Warn: java_basic_ForLoopShouldBeWhileLoop
         for (;true;) true; // No Init or Update part, may as well be: while (true)
